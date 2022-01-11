a52521c1a2b883130f62efecb379bba6e9094f44
vishnu  kumar singh <68421295+devil1329@users.noreply.github.com>
2022-01-11 22:00:25 +0530

    Create q2.cc

---
+++ pattern/q2.cc
@@ -0,0 +1,28 @@
+/*    
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=n-i-1;j>0;j--)
+            cout<<" ";
+        for(j=0;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

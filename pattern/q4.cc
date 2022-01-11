9fdafa1cbc1911a6733d2d4835077d2666e4fd08
vishnu  kumar singh <68421295+devil1329@users.noreply.github.com>
2022-01-11 22:21:23 +0530

    Create q4.cc

---
+++ pattern/q4.cc
@@ -0,0 +1,30 @@
+//     *  
+//    * * 
+//   * * *
+//  * * * *
+// * * * * *
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=5;
+    // cin>>n;
+    int i,j,temp=1;
+    for(i=0;i<n;i++)
+    {
+        for(j=n-i-1;j>0;j--)
+            cout<<" ";
+        int k=temp;
+        for(j=0;k>0;j++)
+        {
+            cout<<"* ";
+            k-=2;
+        }
+        temp+=2;
+        cout<<endl;
+    }
+    return 0;
+}

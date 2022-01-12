954baf1297fb97db10cd9bc09b32e4d72db6d553
vishnu  kumar singh <68421295+devil1329@users.noreply.github.com>
2022-01-13 01:11:48 +0530

    Create q14.cc

---
+++ pattern/q14.cc
@@ -0,0 +1,43 @@
+
+// *
+//  **
+//   ***
+//    ****
+//     *****
+//    ****
+//   ***
+//  **
+// *
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=5;
+    // cin>>n;
+    int i,j;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 2; j <= i; j++) 
+        {
+            cout<<" ";
+        }
+        for (int j = 1; j <= i; j++) 
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for (int i = 1; i <= n-1; i++) {
+        for (int j = n-i-1; j >=1; j--) 
+        {
+            cout<<" ";
+        }
+        for (int j = n-i; j >=1; j--) 
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
